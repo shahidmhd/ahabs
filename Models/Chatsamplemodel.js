45ad97a355a6490fac24af584f0142cb796bf2bb
shahidmhd <shahidvk124@gmail.com>
2023-10-24 17:39:51 +0530

    socket broadcast complete

--- Models/Chatsamplemodel.js
+++ Models/Chatsamplemodel.js
@@ -14,3 +14,27 @@ const chatSchema = new mongoose.Schema({
 const Chat = mongoose.model('Chat', chatSchema);
 
 export default Chat;
+
+
+// import mongoose from 'mongoose';
+
+// const chatSchema = new mongoose.Schema({
+//   senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//   message: String,
+//   roomId: { type: mongoose.Schema.Types.ObjectId, ref: 'Room' },
+//   // Fields for file upload
+//   files: [
+//     {
+//       data: Buffer, // Store binary file data
+//       contentType: String, // Store file content type (e.g., 'image/jpeg', 'video/mp4', 'application/pdf', etc.)
+//       filename: String, // Store the original filename
+//     },
+//   ],
+// }, {
+//   timestamps: true,
+// });
+
+// // Create a Room model using the schema
+// const Chat = mongoose.model('Chat', chatSchema);
+
+// export default Chat;
--- config/Socket.js
+++ config/Socket.js
@@ -1,18 +1,17 @@
 import ChatMessage  from "../Models/Chatsamplemodel.js";
-
 const onlineUsers = new Set();
 
 const initializeSocketIO = (io) => {
-  io.on('connection', (socket) => {
+  io.on('connection',async (socket) => {
     console.log('User connected with socket.id:', socket.id);
     onlineUsers.add(socket.id);
-
+  
     socket.broadcast.emit('user-joined', socket.id);
     console.log(onlineUsers);
 
     socket.on('chat-message', async (message) => {
       console.log(message, 'Received chat message from client');
-
+    
       // Assuming you have a ChatMessage model
       const chatMessage = new ChatMessage({
         senderId: message.userId,
@@ -31,14 +30,11 @@ const initializeSocketIO = (io) => {
         //   timestamp: savedMessage.timestamp,
         // });
 
-        socket.emit('chat-message',savedMessage);
+       socket.broadcast.emit('chat-message',savedMessage);
 
         // Broadcast the message to other users in the room
-        // socket.to(message.roomid).emit('chat-message', {
-        //   savedMessage,
-        //   message: savedMessage.message,
-        
-        // });
+
+        // io.to(message.roomid).emit('chat-message',savedMessage);
       } catch (error) {
         console.error('Error saving message to the database:', error);
       }
@@ -53,3 +49,87 @@ const initializeSocketIO = (io) => {
 };
 
 export default initializeSocketIO;
+
+
+
+// import ChatMessage from "../Models/Chatsamplemodel.js";
+// import path from 'path';
+
+// import { AWS, s3 } from './Awss3.js'
+// const onlineUsers = new Set();
+// import upload from './multerconfig.js'
+
+// const bucketName = 'shahidbucketsample';
+
+// const initializeSocketIO = (io) => {
+//   io.on('connection', (socket) => {
+//     console.log('User connected with socket.id:', socket.id);
+//     onlineUsers.add(socket.id);
+
+//     socket.broadcast.emit('user-joined', socket.id);
+//     console.log(onlineUsers);
+
+//     socket.on('chat-message', async (message) => {
+//       console.log(message, 'Received chat message from client');
+
+//       // const chatMessage = new ChatMessage({
+//       //   senderId: message.userId,
+//       //   message: message.message,
+//       //   roomId: message.roomid,
+//       // });
+
+
+//       // if (message.image) {
+//       //   try {
+//       //     const imageFile = message.image;
+//       //     const imageFileName = `${Date.now()}-${path.basename(imageFile.name)}`;
+//       //     const imageUploadPath = path.join(__dirname, 'path/to/your/upload/folder', imageFileName);
+
+//       //     // Move the image file to the specified path
+//       //     await imageFile.mv(imageUploadPath);
+
+//       //     // Upload the image to S3
+//       //     const params = {
+//       //       Bucket: bucketName,
+//       //       Key: imageFileName,
+//       //       Body: require('fs').createReadStream(imageUploadPath),
+//       //     };
+
+//       //     s3.upload(params, (err, data) => {
+//       //       if (err) {
+//       //         console.error('Error uploading image to S3:', err);
+//       //       } else {
+//       //         // Set the image URL in the chat message as the S3 URL
+//       //         chatMessage.image = data.Location;
+
+//       //         // Save the chat message to the database
+//       //         chatMessage.save()
+//       //           .then((savedMessage) => {
+//       //             console.log('Message saved to the database:', savedMessage);
+
+//       //             // Emit the saved message (including image URL) back to the sender
+//       //             socket.emit('chat-message', savedMessage);
+
+//       //             // Broadcast the message (including image URL) to other users in the room
+//       //             socket.to(message.roomid).emit('chat-message', savedMessage);
+//       //           })
+//       //           .catch((error) => {
+//       //             console.error('Error saving message to the database:', error);
+//       //           });
+//       //       }
+//       //     });
+//       //   } catch (error) {
+//       //     console.error('Error saving image:', error);
+//       //   }
+//       // }
+//     });
+
+//     socket.on('disconnect', () => {
+//       console.log('User disconnected with socket.id:', socket.id);
+//       onlineUsers.delete(socket.id);
+//       socket.broadcast.emit('user-left', socket.id);
+//     });
+//   });
+// };
+
+// export default initializeSocketIO;
